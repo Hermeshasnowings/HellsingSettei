67648c66aaf7a99dfb7b7a097a561fdcf7e635c1
Holly <hollylovesapple@hotmail.com>
2022-05-10 20:38:05 +0100

    removed node modules
    im an idiot and committed node modules

--- seeds/script.js
+++ seeds/script.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const Settei = require('../models/Settei.js');
 
-await mongoose.connect('mongodb://localhost:27017/HellsingSettei', {
+mongoose.connect('mongodb://127.0.0.1:27017/HellsingSettei', {
     useNewURLParser: true,
     useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 10000
+    serverSelectionTimeoutMS: 10000,
+    connectTimeoutMS: 20000
 });
 
 const db = mongoose.connection;
